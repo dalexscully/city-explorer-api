173cbf3b245435ae9ff8adf77b0df4097556d05e
Domaine Scully <dalexscully@gmail.com>
2022-10-28 23:51:30 -0400

    lab10

--- modules/movies.js
+++ modules/movies.js
@@ -1,30 +1,54 @@
 'use strict';
+
 const axios = require('axios');
+let cache = require('./cache.js');
+
+// TODO: need create a key - for data to store
+// TODO: if the things exist AND is in a valid timeframe ...send that data
+// TODO: if the the things DO NOT exist - call API & cache the returned data
 
 async function movieMod(req, res, next){
   try {
-    let cityName = req.query.city_name;
+    let cityName = req.query.query;
 
-    let movieUrl = `http://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName}&language=en-US&page=16include_adult=false`;
-    console.log('getting movie data', movieUrl);
+    // *** KEY CREATION FOR DATA TO STORE***
+    let key = cityName + 'movie';
 
-    let movieData = await axios.get(movieUrl);
+    // *** IF data exist AND is in a valid timeframe(cache[key].timestamp) ... send that data
+    if(cache[key] && (Date.now() - cache[key].timestamp < 50000)){
 
-    let dataToSend = movieData.data.results.map(movie => new Movie(movie));
-    res.status(200).send(dataToSend);
+      console.log('Cache was hit');
+      res.status(200).send(cache[key].data);
+
+    } else{
+      console.log('Cache miss');
+      let url = `http://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName}&language=en-US&page=16include_adult=false`;
+
+      let movieData = await axios.get(url);
+
+      let parsedData = movieData.data.results.map(movie => new Movie(movie));
+
+      // ** ADD API return to CACHE
+      cache[key] = {
+        data: parsedData,
+        timestamp: Date.now()
+      };
+
+      res.status(200).send(parsedData);
+
+    }
 
   } catch (error) {
-    console.log(error);
     next(error);
   }
 }
 
 class Movie {
-  constructor(movieObj) {
+  constructor(movieObj){
     this.title = movieObj.title;
     this.overview = movieObj.overview;
     this.img = movieObj.poster_path;
   }
 }
 
-module.exports= movieMod;
+module.exports = movieMod;
